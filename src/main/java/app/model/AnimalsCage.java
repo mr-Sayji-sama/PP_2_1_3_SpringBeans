3ae0db2fcaaa3005f875ebd0da8138fdfd6bfd36
Sergey <serg_zv@msn.com>
2024-07-18 15:25:00 +0300

    Загрузка

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,31 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    private Animal acat;
+    private Animal adog;
+    private Timer time;
 
     @Autowired
-    private Animal animal;
+    public AnimalsCage(@Qualifier("cat") Animal acat, @Qualifier("dog") Animal adog, Timer time) {
+        this.acat = acat;
+        this.adog = adog;
+        this.time = time;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(acat.toString() +", " + adog.toString());
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return this.time;
     }
 }
